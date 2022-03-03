c28b5fec7272d9dd9cbf6212aa234d6c7253387e
dresch86 <danieljresch0708@gmail.com>
2022-03-03 00:54:54 -0500

    1) Add initial routes; 2) Add config file functionality

---
+++ app/src/main/java/org/requisigner/Configuration.java
@@ -0,0 +1,69 @@
+package org.requisigner;
+
+import java.io.IOException;
+
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import io.vertx.core.json.JsonObject;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.CommandLineParser;
+
+public class Configuration {
+    private int iPort;
+    private String sAddress;
+    private JsonObject joConfig;
+
+    private static final Logger lMainLogger = LogManager.getLogger(Configuration.class.getName());
+
+    public Configuration(String[] args, Options opCLIOps) {
+        try {
+            CommandLineParser clpCLIHandler = new DefaultParser();
+            CommandLine clCmdInput = clpCLIHandler.parse(opCLIOps, args);
+
+            if (clCmdInput.hasOption("c")) {
+                Path pConfigPath = Paths.get(clCmdInput.getOptionValue("c"));
+
+                if (Files.exists(pConfigPath)) {
+                    String sJSONCfg = Files.readString(pConfigPath);
+                    joConfig = new JsonObject(sJSONCfg);
+                } else {
+                    System.exit(1);
+                }
+            } else {
+                lMainLogger.fatal("Configuration file not supplied");
+                System.exit(1);
+            }
+        } catch (ParseException e) {
+            lMainLogger.fatal("CLI Error - " + e.getMessage());
+            System.exit(1);
+        } catch (IOException ioe) {
+            lMainLogger.fatal("File System Error - " + ioe.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public JsonObject getSslConfig() {
+        return joConfig.getJsonObject("system").getJsonObject("http").getJsonObject("ssl");
+    }
+
+    public int getPort() {
+        return iPort;
+    }
+
+    public String getAddress() {
+        return sAddress;
+    }
+
+    public String getTemplatesDir() {
+        return joConfig.getJsonObject("system").getJsonObject("filesystem").getString("templates");
+    }
+}
